97423700c343c634a2d6691096066e6edcd4f3fa
Taha Mohammadzadeh <taha.mohammadzadeh.v@gmail.com>
2024-01-13 11:15:10 +0330

    feat: make compatible wit Android 34

--- flutter_google_street_view/example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ flutter_google_street_view/example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,6 +2,7 @@ package io.flutter.plugins;
 
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
+import io.flutter.Log;
 
 import io.flutter.embedding.engine.FlutterEngine;
 
@@ -12,9 +13,22 @@ import io.flutter.embedding.engine.FlutterEngine;
  */
 @Keep
 public final class GeneratedPluginRegistrant {
+  private static final String TAG = "GeneratedPluginRegistrant";
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
-    flutterEngine.getPlugins().add(new zyz.flutter.plugin.flutter_google_street_view.FlutterGoogleStreetViewPlugin());
-    flutterEngine.getPlugins().add(new io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin());
-    flutterEngine.getPlugins().add(new dev.flutter.plugins.integration_test.IntegrationTestPlugin());
+    try {
+      flutterEngine.getPlugins().add(new zyz.flutter.plugin.flutter_google_street_view.FlutterGoogleStreetViewPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin flutter_google_street_view, zyz.flutter.plugin.flutter_google_street_view.FlutterGoogleStreetViewPlugin", e);
+    }
+    try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin flutter_plugin_android_lifecycle, io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin", e);
+    }
+    try {
+      flutterEngine.getPlugins().add(new dev.flutter.plugins.integration_test.IntegrationTestPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin integration_test, dev.flutter.plugins.integration_test.IntegrationTestPlugin", e);
+    }
   }
 }
